a47c3e06441e68505bef3c286afbdb3e91ac42e9
Harun Guven <harun-626@hotmail.com>
2023-06-30 19:46:10 +0300

    add header with styling

--- components/dashboard/Header.js
+++ components/dashboard/Header.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import Back from '../../assets/back.svg'
+import Notifications from '../../assets/notifications.svg'
+
 const Header = () => {
-  return <header>Header Component</header>
+  return (
+    <header className="flex items-center justify-between	py-5 pe-[3.2rem] ps-[1.88rem]">
+      <Back />
+      <Notifications />
+    </header>
+  )
 }
 
 export default Header
