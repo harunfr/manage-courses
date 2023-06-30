d148ca8c7a966056bc3e0aebba3b7b12e27ced03
Harun Guven <harun-626@hotmail.com>
2023-06-30 14:01:40 +0300

    add style to signin

--- pages/signin.js
+++ pages/signin.js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import { useState } from 'react'
+import Link from 'next/link'
 
 const Signin = () => {
   const router = useRouter()
@@ -8,29 +9,75 @@ const Signin = () => {
 
   const handleSignin = (e) => {
     e.preventDefault()
-    // burada validasyon ve diger islemler gerceklestirilebilir.
-
-    // setEmail(email + 'username')
-    // setPassword(password + '!password')
     router.push('/dashboard')
   }
 
   return (
-    <div>
-      <h1 className="test-tailwind">Signin Page</h1>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleSignin}>Signin</button>
+    <div className="bg-gradient-1 flex min-h-screen flex-col items-center justify-center">
+      <div className="form-shadow rounded-2_5xl px-7_5 pt-10_5 min-w	w-96 min-w-[475px] bg-white pb-11">
+        <h1 className="text-3_5xl mb-9 text-center font-bold uppercase">
+          <span className="special-left-border"></span>
+          Manage Courses
+        </h1>
+        <h2 className="text-1_5xl mb-1 text-center font-semibold uppercase">
+          Sign In
+        </h2>
+        <p className="text-gray-850 mb-12 text-center text-sm">
+          Enter your credentials to access your account
+        </p>
+        <div className="mb-4">
+          <label
+            className="mb-1 block text-sm font-medium text-gray-700"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full rounded-md border px-4 py-3.5 text-xs placeholder-[#CDCDCD] focus:border-blue-500 focus:outline-none"
+            required
+          />
+        </div>
+        <div>
+          <label
+            className="mb-1 block text-sm font-medium text-gray-700"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className=" mb-8 w-full rounded-md border px-4 py-3.5 text-xs font-thin placeholder-[#CDCDCD] focus:border-blue-500 focus:outline-none"
+            required
+          />
+        </div>
+        <button
+          onClick={handleSignin}
+          className="focus:shadow-outline bg-primary mb-7 w-full rounded px-4 py-3 text-sm font-medium uppercase text-white hover:bg-blue-600 focus:outline-none"
+        >
+          Sign In
+        </button>
+        <p className="text-gray-850 text-center text-sm">
+          Forgot your password?
+          <Link
+            className="text-primary font-semibold underline"
+            href="/reset-password"
+          >
+            {' '}
+            Reset Password
+          </Link>
+        </p>
+      </div>
     </div>
   )
 }
