206d2761d2e9f0510596ab74970f6ecc49b9f8c7
Harun Guven <harun-626@hotmail.com>
2023-07-03 14:15:30 +0300

    add link to dashboard/home cards

--- components/dashboard/Home/Home.js
+++ components/dashboard/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Link from "next/link"
 
 import Students from "../../../assets/students-lg.svg"
 import Course from "../../../assets/course-lg.svg"
@@ -10,15 +11,26 @@ import Card from "./Card"
 function Home() {
   return (
     <main className=" flex flex-1 flex-wrap content-start items-start justify-between gap-7 bg-[#FDFDFD] px-8 py-7">
-      <Card Icon={Students} bgColor="#F0F9FF" amount="243" type="students" />
-      <Card Icon={Course} bgColor="#FEF6FB" amount="13" type="course" />
-      <Card
-        Icon={Payments}
-        bgColor="#FEFBEC"
-        amount="556,000"
-        type="payments"
-      />
-      <Card Icon={Users} amount="3" type="users" />
+      <Link href="/dashboard/students">
+        <Card Icon={Students} bgColor="#F0F9FF" amount="243" type="students" />
+      </Link>
+      <Link href="/dashboard">
+        {" "}
+        <Card Icon={Course} bgColor="#FEF6FB" amount="13" type="course" />
+      </Link>
+      <Link href="/dashboard">
+        {" "}
+        <Card
+          Icon={Payments}
+          bgColor="#FEFBEC"
+          amount="556,000"
+          type="payments"
+        />
+      </Link>
+      <Link href="/dashboard">
+        {" "}
+        <Card Icon={Users} amount="3" type="users" />
+      </Link>
     </main>
   )
 }
